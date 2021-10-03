8d0476ce0cad79938c688e293114fa1c8f760c70
Polina Sizintseva <polina.sizintseva@taskdata.ru>
2021-10-03 14:46:50 +0300

    First commit

---
+++ src/main/java/odiora/simplejarapp/SimpleJarAppApplication.java
@@ -0,0 +1,13 @@
+package odiora.simplejarapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleJarAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleJarAppApplication.class, args);
+	}
+
+}
